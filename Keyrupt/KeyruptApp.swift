60c53357db452e3b2fe6b633817af262d23a22e1
plouuf <71836826+plouuf@users.noreply.github.com>
2022-10-16 10:08:18 -0230

    Initial Commit

---
+++ Keyrupt/KeyruptApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyruptApp.swift
+//  Keyrupt
+//
+//  Created by ploouf on 2022-10-16.
+//
+
+import SwiftUI
+
+@main
+struct KeyruptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
